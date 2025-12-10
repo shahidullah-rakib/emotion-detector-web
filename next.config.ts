b8ea11f24bf534b18677da6a2379f513116c40bc
shahidullah-rakib <shsidullarakib@gmail.com>
2025-12-10 15:28:47 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const repoName = "emotion-detector-web";
+
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   reactCompiler: true,
 };
 
